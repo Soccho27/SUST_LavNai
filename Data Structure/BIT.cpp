f58403275b1cf09dd4f97ea86c8a38b2e0cce0ce
Soccho27 <2021331027@student.sust.edu>
2026-05-14 16:56:38 +0600

    upd

--- Data Structure/BIT.cpp
+++ Data Structure/BIT.cpp
@@ -1,18 +1,26 @@
-struct BIT{ // 1-base query
-   int sz;     vector<int> bit;
-   BIT(int n){ sz=n;    bit.resize(sz+1,0);  }
-   void update(int pos, int v){
-      while(pos<=sz){
-         bit[pos]+=v;
-         pos=pos+(pos&(-pos));
-      }
-   }
-   int sum(int pos){    int s=0;
-      while(pos>0){
-         s+=bit[pos];
-         pos=pos-(pos&(-pos));
-      }     
-      return s;
-   }
-   int query(int l, int r)    return sum(r)-sum(l-1);
+const int N = 3e5 + 9;
+
+template <class T>
+struct BIT { //1-indexed
+  int n; vector<T> t;
+  BIT() {}
+  BIT(int _n) {
+    n = _n; t.assign(n + 1, 0);
+  }
+  T query(int i) {
+    T ans = 0;
+    for (; i >= 1; i -= (i & -i)) ans += t[i];
+    return ans;
+  }
+  void upd(int i, T val) {
+    if (i <= 0) return;
+    for (; i <= n; i += (i & -i)) t[i] += val;
+  }
+  void upd(int l, int r, T val) {
+    upd(l, val);
+    upd(r + 1, -val);
+  }
+  T query(int l, int r) {
+    return query(r) - query(l - 1);
+  }
 };
