f700f1d24117e6793a526fede4663665d36c027d
Arman9444 <2023331094@student.sust.edu>
2026-05-14 17:09:12 +0600

    Update centroidDecomposition.cpp

--- Data Structure/centroidDecomposition.cpp
+++ Data Structure/centroidDecomposition.cpp
@@ -6,7 +6,7 @@ struct Tree {
     vector<vector<int>> adj, up;
     vector<int> dep, sub, cpar;
     vector<bool> removed;
- 
+
     void init(int N) {
         n = N;
         adj.assign(n + 1, {});
@@ -22,16 +22,16 @@ struct Tree {
     }
     void dfs_lca(int u, int p) {
         up[u][0] = p;
-        for (int i = 1; i < L; ++i) up[u][i] = up[up[u][i - 1]][i - 1];
+        for (int i = 1; i < L; i++) up[u][i] = up[up[u][i - 1]][i - 1];
         for (int v : adj[u]) if (v != p) {
-            dep[v] = dep[u] + 1;
-            dfs_lca(v, u);
-        }
+                dep[v] = dep[u] + 1;
+                dfs_lca(v, u);
+            }
     }
     int lca(int u, int v) {
         if (dep[u] < dep[v]) swap(u, v);
         int d = dep[u] - dep[v];
-        for (int i = L-1; i>=0; i--) 
+        for (int i = L - 1; i >= 0; i--)
             if ((d >> i) & 1) u = up[u][i];
         if (u == v) return u;
         for (int i = L - 1; i >= 0; i--)
@@ -44,13 +44,13 @@ struct Tree {
     int dfs_size(int u, int p) {
         sub[u] = 1;
         for (int v : adj[u]) if (v != p && !removed[v])
-            sub[u] += dfs_size(v, u);
+                sub[u] += dfs_size(v, u);
         return sub[u];
     }
     int find_centroid(int u, int p, int treeSize) {
         for (int v : adj[u]) if (v != p && !removed[v])
-            if (sub[v] * 2 > treeSize)
-                return find_centroid(v, u, treeSize);
+                if (sub[v] * 2 > treeSize)
+                    return find_centroid(v, u, treeSize);
         return u;
     }
     void build_centroid(int u, int p) {
@@ -59,7 +59,7 @@ struct Tree {
         removed[c] = true;
         cpar[c] = p;
         for (int v : adj[c]) if (!removed[v])
-            build_centroid(v, c);
+                build_centroid(v, c);
     }
     void build(int root = 1) {
         dep[root] = 0;
