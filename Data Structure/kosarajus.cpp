64e7b68934622f718427176ab7c20a1dafbe7645
JACsadi <122394339+JACsadi@users.noreply.github.com>
2026-05-14 17:28:46 +0600

    Implement DFS for Kosaraju's algorithm
    Added depth-first search functions for Kosaraju's algorithm and updated the structure for nodes.

--- Data Structure/kosarajus.cpp
+++ Data Structure/kosarajus.cpp
@@ -33,3 +33,30 @@ void kosaRajus(int n){
         }
     }
 }
+////// amar ta 
+typedef struct node {
+   int vis; vector<int> adj; vector<int> ulta;
+  } node;
+  node tree[300010]; vector<vector<int>> scc(300010); stack<int> st; int scount = 0;
+  void dfs(int u) {
+    tree[u].vis = 1;
+    f(i,tree[u].adj.size()) {
+        int next = tree[u].adj[i];
+        if(tree[next].vis==0) dfs(next);
+    }
+    st.push(u);
+  }
+  void dfs2(int u) {
+    tree[u].vis = 1; scc[scount-1].push_back(u); 
+    f(i,tree[u].ulta.size()) {
+        int next = tree[u].ulta[i];
+        if(tree[next].vis==0) dfs2(next);
+    }
+  }
+ for(int i = 1 ; i <= n;i++) if(tree[i].vis==0){ dfs(i);}
+  while (!st.empty()) {
+    int u = st.top();st.pop();
+    if(tree[u].vis==0) {
+        scount++;   dfs2(u);
+    }
+   };
